327ea6fdda8791987119f3960171ff1d01840249
VanessB <vanessbut@yandex.ru>
2023-09-06 16:01:15 +0300

    New model implementation.

--- source/model.jl
+++ source/model.jl
@@ -21,6 +21,7 @@ function get_initial_conditions()
     # Initial conditions.
     V_0 = 200.0
     E_0 = 256.0
+    W_0 = 0.0
     x_0 = [V_0, E_0]
 
     # History
@@ -33,48 +34,57 @@ end
 function make_model()
     n_physically_informed_params = 5
 
-    rhs_nn = Chain(Dense(2, 50, tanh), Dense(50, 50, tanh), Dense(50, 2)) |> f64
+    # Neural network architecture.
+    rhs_nn = Chain(Dense(2, 10, tanh), Dense(10, 10, tanh), Dense(10, 4)) |> f64
     p_nn, rhs_nn = Flux.destructure(rhs_nn)
     n_nn_params = length(p_nn)
 
+    # Right-hand-side function for differential equation.
     function rhs(dot_x, x, params, t)
+        # Get params and current values.
         beta, K, alpha_E, b_i, gamma = exp.(params[begin:n_physically_informed_params])
         V, E = x
 
-        rhs_nn_output = 0.01 * rhs_nn(x, params[n_physically_informed_params + 1:end])
-        dot_x[1] = beta * V * (1.0 - V / K) - gamma * (1.0 + rhs_nn_output[1]) * V * E
-        dot_x[2] = b_i * (1.0 + rhs_nn_output[2]) * V * E - alpha_E * E
+        # Get NN output (+ scale and center around 1.0).
+        rhs_nn_output = 1.0 .+ 1e-1 * rhs_nn(log.(max.(x, 1e-16)), params[n_physically_informed_params + 1:end])
+
+        # Compute derivative.
+        dot_x[1] = beta * V * (1.0 - V / (K * rhs_nn_output[1])) - gamma * V * E * rhs_nn_output[2]
+        dot_x[2] = b_i * V * E * rhs_nn_output[3] - alpha_E * E * rhs_nn_output[4]
 
         return dot_x
     end
 
+    # Assemble initial parameters vector.
+    # Physically informed parameters are stored in log scale.
     initial_params = zeros(n_physically_informed_params + n_nn_params)
     initial_params[begin:n_physically_informed_params] .= log.([4.5, 2.7e6, 9.3e-2, 9.22e-7, 1.4e-6])
     initial_params[n_physically_informed_params + 1:end] = p_nn
 
-    return rhs, initial_params
+    return rhs, initial_params, rhs_nn
 end
 
 
-function make_problem(t_end = 15.0)
+function make_problem(rhs, initial_params, t_end = 15.0)
     x_0, history = get_initial_conditions()
     t_span = (0.0, t_end)
-    rhs, initial_params = make_model()
     problem = ODEProblem{true}(rhs, x_0, t_span)
 
-    return problem, initial_params
+    return problem
 end
 
 
 function make_neural_ode_loss(problem, T_obs, X_obs)
+    # Unite time grids.
     T = sort(unique(cat(T_obs..., dims=1)))
     T_obs_indexes = [something.(indexin(x, T)) for x in T_obs]
 
+    # Loss function (MSE in log scale).
     function loss_adjoint(params)
         solution = Array(solve(problem, Tsit5(), p=params, saveat=T,
                                sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true))))
 
-        loss = sum(sum(([log(solution[dim,index]) for index in T_obs_indexes[dim]] - log.(X_obs[dim])).^2) for dim=1:size(X_obs)[1])
+        loss = sum(sum(([log(max.(solution[dim,index], 1e-16)) for index in T_obs_indexes[dim]] - log.(X_obs[dim])).^2) for dim=1:2)
         return loss
     end
 
@@ -82,10 +92,7 @@ function make_neural_ode_loss(problem, T_obs, X_obs)
 end
 
 
-function find_optimal()
-    T_obs, X_obs = get_observations()
-    problem, initial_params = make_problem()
-
+function fit_model(problem, initial_params, T_obs, X_obs)
     loss_function = make_neural_ode_loss(problem, T_obs, X_obs)
 
     iter = 0
@@ -101,14 +108,16 @@ function find_optimal()
     adtype = Optimization.AutoZygote()
     optf = Optimization.OptimizationFunction((x,p) -> loss_function(x), adtype)
     optprob = Optimization.OptimizationProblem(optf, initial_params)
-    res1 = Optimization.solve(optprob, Adam(0.05), callback = callback, maxiters = 150)
+    res1 = Optimization.solve(optprob, Adam(0.01), callback = callback, maxiters = 1000)
+
+    println("Decreasing learning rate")
 
     optprob2 = Optimization.OptimizationProblem(optf, res1.u)
-    res2 = Optimization.solve(optprob2, Adam(0.001), callback = callback, maxiters = 150)
-    opt = res2.u
+    res2 = Optimization.solve(optprob2, Adam(0.001), callback = callback, maxiters = 4000)
 
-    solution = solve(problem, Tsit5(), p=opt)
-    draw(solution, opt, T_obs, X_obs[1], X_obs[2], "Нейромодель")
+    println("Finished")
+
+    return res2
 end
 
 
@@ -129,7 +138,25 @@ function draw(solution, p, T_obs, V_obs, E_obs, str_title)
     ylabel!("копии на селезенку")
     title!(str_title)# * ", Φ(p̂) = " * @sprintf("%3.2e", obj(p))) #* ", AICc = " * @sprintf("%4.1f", aic(p)))
 
-    savefig(str_title * ".pdf")
+    savefig("output/" * str_title * ".pdf")
+end
+
+
+function main()
+    # Acquire observations (true data).
+    T_obs, X_obs = get_observations()
+
+    # Assemble model.
+    rhs, initial_params, rhs_nn = make_model()
+    problem = make_problem(rhs, initial_params)
+
+    # Optimize.
+    optimization_result = fit_model(problem, initial_params, T_obs, X_obs)
+    optimal_params = optimization_result.u
+
+    # Draw solution.
+    solution = solve(problem, Tsit5(), p=optimal_params)
+    draw(solution, opt, T_obs, X_obs[1], X_obs[2], "Нейромодель")
 end
 
 
